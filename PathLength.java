3d9a91761dee76b3eae5d4880ab2262f131f5573
Mahathi <mathreya24@amherst.edu>
2022-04-26 22:18:20 -0400

    Added a few classes

---
+++ PathLength.java
@@ -0,0 +1,20 @@
+public class PathLength {
+    Node[] nodes;
+    Edge[] edges;
+
+    public PathLength(Node[] n, Edge[] e) {
+        //can change this
+        nodes = n;
+        edges = e;
+    }
+
+    public Node[] shortestPath(Node n1, Node n2) {
+        //Algorithm
+
+    }
+
+    public Node[] nodesToReturn() {
+
+    }
+
+}
